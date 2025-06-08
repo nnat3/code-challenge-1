e28496864210a3b67527aa40352284e419810c8e
Neathan Kungu <natekungu857@gmail.com>
2025-06-08 21:59:25 +0300

    corrects reference error in chai.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,1 +1,19 @@
 // write code here
+//define a function to calculate fare
+function calculateBodaFare(distanceInKm) {
+    //define base fare and fare per kilometer
+    const baseFare = 50; // in Ksh
+    const chargePerKm = 15; // in Ksh
+
+    //calculate distance fare
+    const distanceFare = distanceInKm * chargePerKm;
+
+    //add base fare to distance fare to acquire total fare
+    const totalFare = baseFare + distanceFare;
+
+    //ask user for travel distance
+    const userInput = prompt(`Unafika wapi mteja? Ingiza umbali wa safari yako kwa kilomita:`);
+
+    //convert user input to a number
+    const distanceInKm= Number(userInput);
+}
--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,17 +1,6 @@
 // Write code here
 // function introduced
 function calculateChaiIngredients(numberofcups) {
-// Prompt user for number of cups
-   const userPreference = prompt('Karibu! How many cups of Chai Bora would you like to make?', );
-// Convert user input to a number
-const numberofcups = Number(userPreference);
-// Check if input is a valid number
-if (isNaN(numberofcups) || numberofcups <= 0) {
-    console.log('Please enter a valid number of cups.');
-}
-else {
-    calculateChaiIngredients(numberofcups);
-}
 
 // list of ingredients per cup
 const waterPerCup = 200; // in ml
@@ -31,5 +20,17 @@ console.log(`Water:${totalWater} ml of water`);
 console.log(`Milk: ${totalMilk} ml of milk`);
 console.log(`Tea Leaves: ${totalTeaLeaves} tablespoons of tea leaves`);
 console.log(`Sugar: ${totalSugar} teaspoons of sugar`);
+
+// Prompt user for number of cups
+   const userPreference = prompt('Karibu! How many cups of Chai Bora would you like to make?', );
+// Convert user input to a number
+const numberofcups = Number(userPreference);
+// Check if input is a valid number
+if (isNaN(numberofcups) || numberofcups <= 0) {
+    console.log('Please enter a valid number of cups.');
+}
+else {
+    calculateChaiIngredients(numberofcups);
+}
 
 }
